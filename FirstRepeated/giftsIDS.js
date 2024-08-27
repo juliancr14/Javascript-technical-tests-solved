a8fec8fe8a920ea0a314073f9a2a3d88239e7acd
jcardonar14 <jcardonar@udistrital.edu.co>
2024-08-27 15:30:47 -0500

    Adding a conditional at return with this the function dont going to return undefined if that is the case

--- FirstRepeated/giftsIDS.js
+++ FirstRepeated/giftsIDS.js
@@ -7,7 +7,7 @@ function findFirstRepeated(gifts) {
     //Este ciclo obtiene los número repetidos en orden inverso y los guarda en el array repeatedI
     for (let i = gifts.length; i >= 0; i--) {
 
-        id = gifts[i];
+        let id = gifts[i];
 
         for (let x = i - 1; x >= 0; x--) {
 
@@ -44,7 +44,12 @@ function findFirstRepeated(gifts) {
     }*/
 
     //Retorna el primer valor repetido
-    return firstRepeated;
+    if (firstRepeated === undefined) {
+        return -1;
+    } else {
+        return firstRepeated;
+    }
+
 }
 
 const giftIds = [2, 1, 3, 5, 3, 2];
@@ -57,4 +62,8 @@ console.log(firstRepeatedId2);
 
 const giftIds3 = [5, 1, 5, 1];
 const firstRepeatedId3 = findFirstRepeated(giftIds3);
-console.log(firstRepeatedId3);2
+console.log(firstRepeatedId3);
+
+const giftIds4 = [];
+const firstRepeatedId4 = findFirstRepeated(giftIds4);
+console.log(firstRepeatedId4);
