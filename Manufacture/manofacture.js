f45b23a95315a5d741245c6db51511e3c875d7af
jcardonar14 <jcardonar@udistrital.edu.co>
2024-09-03 11:33:57 -0500

    Added a new temporaly unsolved technical test

---
+++ Manufacture/manofacture.js
@@ -0,0 +1,23 @@
+function manufacture(gifts, materials) {
+
+    let separedMaterials = materials.split("");
+
+    for (let i = 0; i < gifts.length; i++) {
+
+        let separedGift = gifts[i].split("");
+
+        for (let x = 0; x < separedMaterials.length; x++) {
+
+            if (separedGifts[x]) {
+
+            }
+        }
+    }
+
+    return [];
+}
+
+const gifts = ['tren', 'oso', 'pelota'];
+const materials = 'tronesa';
+
+manufacture(gifts, materials);
